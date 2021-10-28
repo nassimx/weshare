4ef444a27322954aaadf3ad66aca1dd526a29ee4
Nassim Frikha <nassim@Nassims-Air-2.home>
2021-10-28 10:23:22 +0100

    first commit

---
+++ server/routes/conversations.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const Conversation = require('../models/Conversation');
+// new Conversation
+router.post('/', async (req, res) => {
+  const newConversation = new Conversation({
+    members: [req.body.senderId, req.body.receiverId],
+  });
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(200).send(savedConversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// get conv of a user
+router.get('/:userId', async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
